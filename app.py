15585d19128d622c9c7c9a5cbb7e16a65286b629
Josue Rivera <122042206@upq.edu.mx>
2025-02-25 20:28:23 -0600

    primer commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import mysql.connector
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():# put application's code here
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database="db"
+    )
+    cursor = conn.cursor()
+    cursor.execute("select * from students")
+    students = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('index.html', students=students)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
